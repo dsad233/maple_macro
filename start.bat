6953b9e5898fa88286b8f92b08b4a2e77cf6d895
dsad233 <ksl1421@naver.com>
2025-11-15 18:05:06 +0900

    feat: batch 파일 추가

---
+++ start.bat
@@ -0,0 +1,33 @@
+@echo off
+@chcp 65001
+
+ :: BatchGotAdmin
+ :-------------------------------------
+ REM  --> Check for permissions
+ >nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+
+REM --> If error flag set, we do not have admin.
+ if '%errorlevel%' NEQ '0' (
+     echo Requesting administrative privileges...
+     goto UACPrompt
+ ) else ( goto gotAdmin )
+
+:UACPrompt
+     echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+     echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+     exit /B
+
+:gotAdmin
+     if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
+     pushd "%CD%"
+     CD /D "%~dp0"
+ :--------------------------------------
+
+title 메이플 매크로
+
+echo 메이플 매크로 실행중...
+poetry run python maple_auto.py
+
+pause
